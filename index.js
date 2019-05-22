16a3767e808d7ae3c52640b0052f2721e6a56944
roger gc <nppcpp@hotmail.com>
2019-05-22 22:04:06 +0200

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+import ReactDom from 'react-dom'
+import React from 'react'
+
+import 'favicon/android-chrome-192x192'
+import 'favicon/android-chrome-512x512'
+import 'favicon/apple-touch-icon'
+import 'favicon/favicon-16x16'
+import 'favicon/favicon-32x32'
+import 'favicon/favicon'
+import 'favicon/site'
+
+ReactDom.render
+(
+  <div>hello</div>,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+//wbclient.config.js
+
+const path = require('path')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin= require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports =
+{
+  entry:'./index.js',
+  module:
+  {
+    rules:
+    [
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use:
+        {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|webmanifest)$/,
+        use:
+        [
+          {
+            loader: 'file-loader',
+            options:
+            {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use:
+        {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        exclude: /node_modules/,
+        loaders:
+        [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins:
+  [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin
+    (
+      {
+        template: './index.html'
+      }
+    ),
+    new MiniCssExtractPlugin()
+  ],
+  resolve:
+  {
+    modules:
+    [
+      "node_modules",
+      path.resolve(__dirname, "./")
+    ],
+    extensions: ['.js','.css','.scss','.sass','.ico','.png','.webmanifest']
+  }
+}
